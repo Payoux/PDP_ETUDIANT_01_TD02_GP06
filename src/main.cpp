40482313e514623eb578402ffb95f55d1c39d5b9
Gaspard MONTANTEME <gaspard.montanteme@gmail.com>
2024-01-22 11:33:52 +0100

    e2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 
+// Define the pins that we will use
+#define SENSOR 33
+#define LED 26
+
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // put your code here to run repeatedly
+  // Effectuer une mesure toutes les 5s
+  delay(5000);
+
+  // Afficher l'humidité relative avec un peu de formatage
+
+
+
+  // Afficher la température avec un peu de formatage
 }
